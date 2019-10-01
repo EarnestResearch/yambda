87528f64fc66ce2aa48b92a46df01a38e07477e7
Alessandro Marrella <amarrella@earnestresearch.com>
2019-10-01 20:37:39 +0100

    Add JSON handler

---
+++ src/AWS/Lambda/JsonHandler.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
+module AWS.Lambda.JsonHandler where
+
+import           AWS.Lambda.RuntimeClient
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.Logger
+import qualified Data.Aeson as A
+import           Data.Text
+
+jsonHandler :: (MonadIO m, MonadLogger m, A.FromJSON e, Show e, Show r) => (e -> r) -> m ()
+jsonHandler f = forever $ do
+  client <- runtimeClient A.eitherDecode
+  forever $ handle client f
+
+handle :: (MonadIO m, MonadLogger m, A.FromJSON e, Show e, Show r) => RuntimeClient e r m -> (e -> r) -> m ()
+handle RuntimeClient{..} f = do
+  event@Event{..} <- getNextEvent
+  case eventBody of
+    Right val -> postResponse eventID (f val)
+    Left e    -> postError eventID (Error "Parse failure" $ pack e)
