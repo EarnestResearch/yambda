3f348b515c982da96d8dfd33c6e0c74e48c9114e
Andrey Kartashov <andrey.kartashov@gmail.com>
2020-05-06 19:05:18 -0400

    Change error handling logic.
    Use unliftio package to catch all synchronous exceptions instead of catchError.
    In practice in our examples catchError specialized to handling IOException
    *only*. However, the intention of the code is to run in a "catch all" loop
    reporting what we can back via API.
    
    An example of a problem *not* handled with catchError:
    
    > catchError
    >   (do print (Prelude.head ([] :: [String])) ; pure ())
    >   (\ ioe -> putStrLn ("HAHA caught ::: " ++ show ioe))

--- core/src/AWS/Lambda/Handler.hs
+++ core/src/AWS/Lambda/Handler.hs
@@ -1,26 +1,51 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 
-module AWS.Lambda.Handler where
+module AWS.Lambda.Handler
+  ( handler
+  , handle
+  ) where
 
 import           AWS.Lambda.Encoding
 import           AWS.Lambda.RuntimeClient
 import           Control.Monad
-import           Control.Monad.Except
 import           Control.Monad.Logger
 import qualified Data.Aeson as A
-import           Data.Text
+import qualified Data.Text as T
+import           UnliftIO hiding (handle)
 
-handler :: (LambdaEncode err, MonadError err m, MonadIO m, MonadLogger m, LambdaDecode e, LambdaEncode r) => (e -> m r) -> m ()
+
+-- | Loop forever handling events with a default runtime client and a given callback
+handler
+  :: MonadUnliftIO m
+  => MonadLogger m
+  => LambdaDecode e
+  => LambdaEncode r
+  => (e -> m r)
+  -> m ()
 handler f = forever $ do
   client <- runtimeClient
   forever $ handle client f
 
-handle :: (LambdaEncode err, MonadError err m, MonadIO m) => RuntimeClient e r m -> (e -> m r) -> m ()
-handle RuntimeClient{..} f = do
-  Event{..} <- getNextEvent
-  let
-    result = case eventBody of
-      Right val -> f val >>= postResponse eventID
-      Left e    -> postError eventID (Error "Parse failure" $ (A.String . pack) e)
-  catchError result (postError eventID . Error "Error" . encodeOutput)
+
+-- | Handle single event with a given runtime client and a callback
+handle
+  :: MonadUnliftIO m
+  => MonadLogger m
+  => RuntimeClient e r m
+  -> (e -> m r)
+  -> m ()
+handle RuntimeClient{..} f =
+  tryHandleEvent `catchAny` \e ->
+    logErrorN (tshow e) -- on this level there's no event ID to report but try to at least log
+
+  where
+    tshow = T.pack . show
+
+    tryHandleEvent = do
+      Event{..} <- getNextEvent
+
+      handleAny (postError eventID . Error "Call failed" . A.String . tshow) $
+        case eventBody of
+          Right val -> f val >>= postResponse eventID
+          Left e    -> postError eventID (Error "Failed to parse event" $ (A.String . T.pack) e)
