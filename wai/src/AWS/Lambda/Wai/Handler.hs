59348ecad949b0db07e2b4ad8bd8b3339126a156
Andrey Kartashov <andrey.kartashov@gmail.com>
2020-05-12 18:12:36 -0400

    WAI encoding/decoding

--- wai/src/AWS/Lambda/Wai/Handler.hs
+++ wai/src/AWS/Lambda/Wai/Handler.hs
@@ -1,12 +1,27 @@
+{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
 
-module AWS.Lambda.Wai.Handler where
+{- | Adapts API Gateway models to WAI request/response.
+
+     The goal is to support packaging small, infrequently
+     used JSON APIs as Lambdas using existing WAI-compatible
+     frameworks.
+
+     Note that this conversion is only an approximation and
+     many things possible in HTTP are not representable
+     (e.g. streaming).
+-}
+module AWS.Lambda.Wai.Handler
+  ( handleWaiApplication
+  ) where
+
 
 import           AWS.Lambda.Event.APIGatewayV2
 import           AWS.Lambda.RuntimeClient
 import           AWS.Lambda.Wai.Encoding
+import           Control.Lens
 import           Control.Monad
 import           Control.Monad.Logger
 import qualified Data.Aeson as JSON
@@ -15,6 +30,7 @@ import           Network.Wai.Internal (ResponseReceived (..))
 import           UnliftIO
 
 
+-- | Wraps a Lambda execution layer around a WAI app
 handleWaiApplication
   :: forall m
    . MonadUnliftIO m
@@ -47,13 +63,14 @@ handleWaiApplication waiApp = do
       :: HTTPRequest JSON.Value
       -> (HTTPResponse JSON.Value -> m ())
       -> m ()
-    handleWaiApp req postLambdaResponse = void $ withRunInIO $ \rio ->
+    handleWaiApp req postLambdaResponse = void $ withRunInIO $ \rio -> do
+      waiReq <- gwReqToWai req
       waiApp  -- Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
-      (gwReqToWai req)
-      (\httpRes -> do rio $ postLambdaResponse (waiResToGw httpRes)
-                      pure ResponseReceived)
-
+        waiReq
+        (\httpRes -> do lamRes <- waiResToGw httpRes
+                        rio $ postLambdaResponse lamRes
+                        pure ResponseReceived)
 
 
 serverErrorResponse :: HTTPResponse JSON.Value
-serverErrorResponse = undefined
+serverErrorResponse = httpResponse 500 & body ?~ JSON.String "Server error"
